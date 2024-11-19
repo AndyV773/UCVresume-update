19759fb92b68764b7bee4644e6f20c65aeb0583e
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-11-19 14:09:39 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,5 +1,4 @@
 function userInformationHTML(user) {
-    let fixedAvatar = user.avatar_url.replace(" ", "/");
     return `
     <h2>${user.name}
         <span class="small-name">
@@ -8,13 +7,33 @@ function userInformationHTML(user) {
     </h2>
     <div class="gh-content">
         <div class="gh-avatar">
-            <a href="${user.html_url}" target="_blank>
-                <img src="${fixedAvatar}" width="80" height="80" alt="${user.login}">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
             </a>
         </div>
         <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>`;
-    console.log(user.avatar_url);
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+
+    var listItemsHTML = repos.map(function (repo) {
+        return `<li>
+                <a href="${repo.html_url}" traget="_blank">${repo.name}</a>
+            </li>`
+    });
+
+    return `<div class="clearfix repo-list">
+            <p>
+                <strong>Repo List:</strong>
+            </p>
+            <ul>
+                ${listItemsHTML.join("\n")}
+            </ul>    
+        </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -34,10 +53,12 @@ function fetchGitHubInformation(event) {
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        }, function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
